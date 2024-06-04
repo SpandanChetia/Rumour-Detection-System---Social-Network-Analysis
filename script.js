97cdc8dd69f4579bf7fee733d2e90facf0c5faee
SpandanPriyam22 <spandanpriyam1976@gmail.com>
2024-06-05 02:38:38 +0530

    Initial files added

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let rumournodes = [];
 
 const createForms = (totalEdges) => {
     const edgeCollection = document.querySelector('#input-container');
-    edgeCollection.innerHTML = ''; // Clear previous inputs
+    edgeCollection.innerHTML = ''; 
     for (let i = 1; i <= totalEdges; i++) {
         let sp = document.createElement('span');
         let ip = document.createElement('input');
@@ -13,23 +13,25 @@ const createForms = (totalEdges) => {
         ip.type = "text";
         ip.placeholder = `Enter the edge no. ${i}`;
         
-        sp.style.display = 'flex';
-        sp.style.flexDirection = 'row';
+        ip.classList.add('edge-input');
+        sp.classList.add('edge-container'); 
         sp.append(ip);
         
         edgeCollection.append(sp); 
     }
     const expectedHeight = totalEdges * 12; 
 
-    // Set min-height before appending
     edgeCollection.style.minHeight = `${expectedHeight}px`;
 
     const edgeSubmit = document.createElement('button');
     edgeSubmit.textContent = 'Submit';
     edgeSubmit.id = 'final_submit';
 
+    edgeSubmit.classList.add('edge-submit-btn');
+
     edgeCollection.append(edgeSubmit);
 };
+
 
 const submitEdges = () => {
     const totalEdges = document.querySelector('#edge-input').value;
@@ -42,7 +44,7 @@ const submitEdges = () => {
         return;
     }
     createForms(Number(totalEdges));
-    edges = []; // Reset edges array
+    edges = [];
     const edgeSub = document.querySelector('#final_submit');
     edgeSub.addEventListener('click', () => {
         for (let i = 1; i <= totalEdges; i++) {
@@ -62,7 +64,7 @@ const submitEdges = () => {
             errorMessage.style.display = 'none';
         }
         document.querySelector("#graph-container").innerHTML = "";
-        visualizeGraph(); // Call the function to visualize the graph
+        visualizeGraph(); 
         rumourspread();
     });
 };
@@ -93,8 +95,12 @@ const rumourspread = () => {
         return;
     }
     rumourContainer.style.display = 'block';
+    rumourContainer.style.marginBottom = '20px';
     const rumourbtn = document.querySelector('#Rumourbtn');
-    rumourbtn.addEventListener('click', rumourfetching);
+    rumourbtn.addEventListener('click', ()=>{
+        rumourfetching();
+        viewResults();
+    });
 }
 
 const visualizeGraph = () => {
@@ -141,9 +147,9 @@ const visualizeGraph = () => {
         .attr("r", 10)
         .attr("fill", d => {
             if (rumournodes.length > 0) {
-              return rumournodes.includes(d.id+1) ? "red" : "steelblue";
+              return rumournodes.includes(d.id+1) ? "red" : "rgb(98, 9, 176)";
             } else {
-              return "steelblue";
+              return "rgb(98, 9, 176)";
             }
           })
         .call(d3.drag()
@@ -194,5 +200,172 @@ const visualizeGraph = () => {
         d.fx = null;
         d.fy = null;
     }
-}
+};
 
+const permute = (nums) => {
+    let results = [];
+
+    if (nums.length === 0) return [];
+    if (nums.length === 1) return [nums];
+
+    for (let i = 0; i < nums.length; i++) {
+        const currentNum = nums[i];
+        const remainingNums = nums.slice(0, i).concat(nums.slice(i + 1));
+        const remainingNumsPermuted = permute(remainingNums);
+
+        for (let j = 0; j < remainingNumsPermuted.length; j++) {
+            const permutedArray = [currentNum].concat(remainingNumsPermuted[j]);
+            results.push(permutedArray);
+        }
+    }
+    return results;
+};
+
+const generatePermutations = ()=>{
+    let allPermutations = permute(rumournodes);
+
+    let map = new Map();
+
+    allPermutations.forEach(perm =>{
+        let f = perm[0];
+        if(!map.has(f)){
+            map.set(f, []);
+        }
+        map.get(f).push(perm);
+    });
+
+    return map;
+};
+
+const generateEdges = ()=>{
+    const rumourSet = new Set(rumournodes);
+
+    const allrumourEdges = new Set();
+
+    edges.forEach((edge)=>{
+        const [a, b] = edge;
+
+        if(rumourSet.has(a) && rumourSet.has(b)){
+            const edge1 = `${a},${b}`;
+            const edge2 = `${b},${a}`;
+
+            allrumourEdges.add(edge1);
+            allrumourEdges.add(edge2);
+        }
+    });
+
+    const result = Array.from(allrumourEdges).map(edge => edge.split(',').map(Number));
+    return result;
+};
+
+const bfs_distance = (start, end, adjList) =>{
+    let distance = 0;
+
+    const vis = new Set();
+    let queue = [];
+
+    vis.add(start);
+    queue.push(start);
+    while(queue.length>0){
+        let size = queue.length;
+        for(let i=0; i<size; i++){
+            let node = queue.shift();
+
+            if(node === end){
+                return distance;
+            }
+
+            let neighbours = adjList.get(node) || [];
+            neighbours.forEach((nx)=>{
+                if(!vis.has(nx)){
+                    queue.push(nx);
+                    vis.add(nx);
+                }
+            });
+            distance++;
+        }
+    }
+
+    return -1;
+};
+
+const countPP = (key, value, adjList, rumouredges)=>{
+    let count = 0;
+
+    value.forEach((perm)=>{
+        rumouredges.forEach((edge)=>{
+            let f = edge[0];
+            let s = edge[1];
+            
+            let a = bfs_distance(key, f, adjList);
+            let b = bfs_distance(key, s, adjList);
+            if(a!==-1 && b!==-1 && a < b && perm.indexOf(f) < perm.indexOf(s)){
+                count++;
+            }else if(a!==-1 && b!==-1 && a < b && perm.indexOf(f) >= perm.indexOf(s)){
+                return 0;
+            }
+        });
+    })
+    
+    return count;
+    
+};
+
+const adjacencyList = ()=>{
+    let graph = new Map();
+
+    edges.forEach((edge)=>{
+        const [a, b] = edge;
+
+        if(!graph.has(a)){
+            graph.set(a, []);
+        }
+        graph.get(a).push(b);
+
+        if(!graph.has(b)){
+            graph.set(b, []);
+        }
+        graph.get(b).push(a);
+    });
+
+    return graph;
+};
+
+const permittedPermutation = ()=>{
+    const map = generatePermutations();
+    const rumouredges = generateEdges();
+    const adjList = adjacencyList();
+    
+    let result = new Map();
+    map.forEach((value, key)=>{
+        let count = countPP(key, value, adjList, rumouredges);
+        result.set(key, count);
+    });
+
+    let ans = [];
+    let maxi=0;
+    result.forEach((value, key)=>{
+        if(maxi < value){
+            maxi = value;
+            ans = [];
+            ans.push(key);
+        }else if(maxi === value){
+            ans.push(key);
+        }
+    })
+
+    return ans;
+};
+
+const viewResults = ()=>{
+    let sourceNodes = permittedPermutation();
+    let result_section = document.querySelector('#source');
+    let result = "";
+
+    for(let i=0; i<sourceNodes.length-1; i++){
+        result += sourceNodes[i] + ',';
+    }
+    result += sourceNodes[sourceNodes.length-1];
+    result_section.textContent = result;
+    result_section.style.color = 'red';
+};
